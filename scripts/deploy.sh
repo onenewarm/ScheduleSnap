a7ba819589982561979ef317f3d94427a027501e
onenewarm <imhappywithyou@naver.com>
2023-05-17 15:01:09 +0900

    TEST : Jenkins 연동
    * Git Actions 에서 Jenkins로 변경 합니다.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+## 이전 배포 파일을 지운다.
+#SINCE=`date --date '-4 weeks -2 days' +%F 2>/dev/null || date -v '-4w' -v '-2d' +%F` # day +30일
+#bucket=schedulesnapbucket # s3 bucket
+#aws s3api list-objects-v2 --bucket $bucket --query 'Contents[?LastModified < `'"$SINCE"'`]' --output  text > file-of-keys # s3에서 최종수정일(LastModified)이 기준으로 오래된 파일 조회하여 파일 목록 생성
+#cat file-of-keys | awk '{print $2}' | xargs -P8 -n1000 -L 1 bash -c 'aws s3 rm s3://'$bucket'/$0' # 파일을 읽어와서 | 2번째 컬럼(파일명)만 추려내어 | 1line씩 bash 스크립트 실행 | 실행할 명령은 s3 rm =^ㅅ^=
+
 REPOSITORY=/home/ec2-user/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
