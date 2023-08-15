06d4e47736ce1f88865f4179d637a7b1333f0334
Wonlae Kim <imhappywithyou@naver.com>
2023-08-15 22:22:26 +0900

    FIX : build 오류수정

--- Chatgpt_API_Server/src/test/java/api/Main/DemoApplicationTests.java
+++ Chatgpt_API_Server/src/test/java/api/Main/DemoApplicationTests.java
@@ -1,9 +1,8 @@
-package com.example.demo;
+package api.Main;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
