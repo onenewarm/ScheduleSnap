3e7fc1388197238fba61ca85039a505c0134eeec
Wonlae Kim <imhappywithyou@naver.com>
2023-08-16 19:05:03 +0900

    FIX : 배포 오류 수정
    - build.gradle plain jar 파일 생 성 안되게 했습니다..
    
    - bash 스크립트 ec2-user -> ubuntu로 변경

--- Chatgpt_API_Server/scripts/deploy.sh
+++ Chatgpt_API_Server/scripts/deploy.sh
@@ -6,7 +6,7 @@
 #aws s3api list-objects-v2 --bucket $bucket --query 'Contents[?LastModified < `'"$SINCE"'`]' --output  text > file-of-keys # s3에서 최종수정일(LastModified)이 기준으로 오래된 파일 조회하여 파일 목록 생성
 #cat file-of-keys | awk '{print $2}' | xargs -P8 -n1000 -L 1 bash -c 'aws s3 rm s3://'$bucket'/$0' # 파일을 읽어와서 | 2번째 컬럼(파일명)만 추려내어 | 1line씩 bash 스크립트 실행 | 실행할 명령은 s3 rm =^ㅅ^=
 
-REPOSITORY=/home/ec2-user/app
+REPOSITORY=/home/ubuntu/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
